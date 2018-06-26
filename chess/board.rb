de0f9d1be73ec13f83059ff87cffdb33357763ac
mrn33 <m.najafi@protonmail.com>
2018-06-25 17:30:22 -0700

    Complete phase 1 of board.rb

--- chess/board.rb
+++ chess/board.rb
@@ -1,6 +1,6 @@
 
 class Board
-  attr_reader :piece
+  attr_accessor :grid
 
   def initialize(grid = nil)
     @grid = grid
@@ -9,15 +9,21 @@ class Board
 
   def [](pos)
     row, col = pos
-    @grid[row][col]
+    self.grid[row][col]
   end
 
   def []=(pos, piece)
-    @grid[pos] = piece
+    row, col = pos
+    grid[row][col] = piece
   end
 
   def move_piece(start_pos, end_pos)
-    raise "Invalid Move" if
+    raise "No Piece in Start Position" if self[start_pos].nil?
+    if end_pos.first > grid.length || end_pos.last > grid.transpose.length
+      raise "Position Not on Board"
+    end
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil
   end
 
 end
@@ -30,4 +36,15 @@ end
 class NullPiece < Piece
   def initialize
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  board = Board.new
+  test_piece = Piece.new
+  start_pos = [3, 3]
+  end_pos = [6, 5]
+  board[start_pos] = test_piece
+  p board.grid
+  board.move_piece(start_pos, end_pos)
+  p board.grid
 end
